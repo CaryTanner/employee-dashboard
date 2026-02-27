a93d2e820298a435620d44eb261356d5a89b893a
CaryTanner <cary.tanner@qvalia.com>
2026-02-27 14:22:05 +0100

    Set up app config, tailwind, fonts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { EmployeeList } from './views/employee-list/employee-list';
+import { EmployeeDetails } from './views/employee-details/employee-details';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: App,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'employee-list' },
+      { path: 'employee-list', component: EmployeeList },
+      {
+        path: 'employee/:id',
+        component: EmployeeDetails,
+      },
+    ],
+  },
+];
